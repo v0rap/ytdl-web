8338b774ecc1488125552ff71941535d045628cb
Kim Karlsson <me@thvxl.se>
2022-12-20 16:38:06 +0100

    Add depends on api_lambda

--- main.tf
+++ main.tf
@@ -123,8 +123,7 @@ resource "aws_lambda_function" "api_lambda" {
 
 resource "aws_api_gateway_deployment" "ytdl_api" {
   depends_on = [
-    "aws_api_gateway_integration.lambda",
-    "aws_api_gateway_integration.lambda_root",
+    "aws_api_gateway_integration.api_lambda"
   ]
 
   rest_api_id = "${aws_api_gateway_rest_api.ytdl.id}"
