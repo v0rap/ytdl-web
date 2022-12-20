5bd697915cd128f6e5ebaabedbd268047fdf51ac
Kim Karlsson <me@thvxl.se>
2022-12-20 13:40:06 +0100

    Create Elastic Container Repository

--- main.tf
+++ main.tf
@@ -18,6 +18,15 @@ provider "aws" {
   region = "eu-north-1"
 }
 
+resource "aws_ecr_repository" "ytdl_repo" {
+  name                 = "ytdl"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
 resource "aws_iam_role" "iam_ytdl" {  # Used to only allow access to lambda for the function
   name               = "iam_ytdl"
   assume_role_policy = <<EOF
@@ -37,11 +46,12 @@ resource "aws_iam_role" "iam_ytdl" {  # Used to only allow access to lambda for 
 EOF
 }
 
+/*
 resource "aws_lambda_function" "api_lambda" {
   image_uri     = "ghcr.io/v0rap/ytdl-web:latest"
   function_name = "ytdl"
-  package_type = "Image"
+  package_type  = "Image"
   role          = aws_iam_role.iam_ytdl.arn
   handler       = "app.handler"
-  runtime = "python3.9"
-}
+  runtime       = "python3.9"
+} */
