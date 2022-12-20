798183aa6dcaac31ecdddac9809038431d2b6be8
Kim Karlsson <me@thvxl.se>
2022-12-20 16:06:47 +0100

    Change version to image_version since it's reserved

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-variable "version" {
+variable "image_version" {
   type = string
 }
 
@@ -87,7 +87,7 @@ EOF
 
 
 resource "aws_lambda_function" "api_lambda" {
-  image_uri     = "981644780922.dkr.ecr.eu-north-1.amazonaws.com/ytdl:${var.version}"
+  image_uri     = "981644780922.dkr.ecr.eu-north-1.amazonaws.com/ytdl:${var.image_version}"
   function_name = "ytdl"
   package_type  = "Image"
   role          = aws_iam_role.iam_ytdl.arn
