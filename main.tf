10bd79bbcea3fc0e4149d6c594b1beb953d02e80
Kim Karlsson <me@thvxl.se>
2022-12-20 16:04:59 +0100

    Merge build stages to pass version to Lambda

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+variable "version" {
+  type = string
+}
+
 terraform {
   required_providers {
     aws = {  # AWS Provider for... well... AWS stuff
@@ -83,7 +87,7 @@ EOF
 
 
 resource "aws_lambda_function" "api_lambda" {
-  image_uri     = "981644780922.dkr.ecr.eu-north-1.amazonaws.com/ytdl:v0.0.1"
+  image_uri     = "981644780922.dkr.ecr.eu-north-1.amazonaws.com/ytdl:${var.version}"
   function_name = "ytdl"
   package_type  = "Image"
   role          = aws_iam_role.iam_ytdl.arn
